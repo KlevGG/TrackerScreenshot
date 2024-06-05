7e94f0d1c25f0a855dccd34c9c09ed7d0b4a4a3f
SavageCore <talk@savagecore.uk>
2024-06-05 04:23:10 +0100

    BTN Support

--- screenshots.py
+++ screenshots.py
@@ -651,6 +651,49 @@ if "HDT" in config['wanted-trackers']['trackers']:
     driver.save_screenshot("screenshots/HDT_"+ datastring +".png")
     print("HDT Screenshoted")
 
+if "BTN" in config['wanted-trackers']['trackers']:
+    print("Entering BroadcasTheNet")
+    username = config['broadcasthenet']['username']
+    password = config['broadcasthenet']['password']
+    profile_url = config['broadcasthenet']['profile_url']
+
+    driver.get(profile_url)
+    time.sleep(3) #wait 3 seconds to make sure the page loads
+
+    #Navigation - DO NOT CHANGE
+    username_field = driver.find_element(By.ID, "username")
+    password_field = driver.find_element(By.ID, "password")
+
+    #Send username and password
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+    driver.find_element(By.NAME, "login").click()
+    time.sleep(3)
+
+    if driver.find_elements(By.ID, "challenge-running"):
+        print("Please solve the challenge and press enter to continue or type 'n' and press enter to skip.")
+        response = input()
+        if response == "n":
+            skip = True
+
+    if not skip:
+        #2FA is enabled, ask for the code
+        if driver.find_elements(By.ID, "code"):
+            code = input("Please enter the 2FA code: ")
+            code_field = driver.find_element(By.ID, "code")
+            code_field.send_keys(code)
+            driver.find_element(By.CSS_SELECTOR, "input[type=submit]").click()
+            time.sleep(4)
+
+        driver.get(profile_url)
+        time.sleep(3)
+        driver.find_element(By.LINK_TEXT, "Info").click()
+        time.sleep(3)
+        #Login and save screenshot
+        datastring = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        driver.save_screenshot("screenshots/BroadcasTheNet_"+ datastring +".png")
+        print("BroadcasTheNet Screenshoted")
+
 else:
     driver.quit()
 
